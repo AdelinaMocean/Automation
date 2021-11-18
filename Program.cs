3150336cfaeba2a54f20843ca380c6c8ba9fe385
AdelinaMocean <mocean.adelina94@gmail.com>
2021-11-18 21:16:58 +0200

    Update

--- Program.cs
+++ Program.cs
@@ -7,30 +7,12 @@ namespace Yahoo
 {
     class Program
     {
-        public Program(IWebDriver driver)
-        {
-            _driver = driver;
-        }
-        private IWebDriver _driver { get; set; }
         static void Main(string[] args)
         {
 
-            Console.WriteLine("EnterUsername");
-            var userName = Console.ReadLine();
-            Console.WriteLine(userName);
             
         }
-        public void Login()
-        {
 
-          
-            _driver.Manage().Window.Maximize();
-            _driver.Url = "https://www.google.com/";
-            Console.WriteLine("Enter text");
-     
-          IWebElement insertSerchText = _driver.FindElement(By.ClassName("gLFyf gsfi"));
-            
-        }
 
     }
     
